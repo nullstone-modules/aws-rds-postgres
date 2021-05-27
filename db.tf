e375f0d0e5efaab51a0e61ceeb0a46b007ddf037
Brad Sickles <brad.sickles@gmail.com>
2021-05-27 09:29:52 -0400

    Hooking up backup_retention_period var

--- db.tf
+++ db.tf
@@ -17,7 +17,7 @@ resource "aws_db_instance" "this" {
 
   final_snapshot_identifier = local.resource_name
 
-  backup_retention_period = 7
+  backup_retention_period = var.backup_retention_period
   backup_window           = "02:00-03:00"
 
   tags = data.ns_workspace.this.tags
