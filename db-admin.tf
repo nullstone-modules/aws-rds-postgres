0dd6c8729465f24f21c7dfaf474b91b98012cd40
Brad Sickles <brad.sickles@gmail.com>
2021-07-30 14:19:23 -0400

    Using built-in archive file

--- db-admin.tf
+++ db-admin.tf
@@ -1,10 +1,6 @@
 module "db_admin" {
   source = "api.nullstone.io/nullstone/aws-pg-db-admin/aws"
 
-  // TODO: Awaiting approval from AWS to get the custom alias 'nullstone' instead of 'k6p1g1l8'
-  //       Remove this line when approval is confirmed
-  image_uri = "public.ecr.aws/k6p1g1l8/pg-db-admin:latest"
-
   name     = local.resource_name
   tags     = data.ns_workspace.this.tags
   host     = aws_db_instance.this.address
