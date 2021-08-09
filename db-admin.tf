7d06405babe220409fb977493d5f9be1a473dfe2
Brad Sickles <brad.sickles@gmail.com>
2021-08-09 17:33:26 -0400

    Upgrade pg-db-admin

--- db-admin.tf
+++ db-admin.tf
@@ -1,6 +1,6 @@
 module "db_admin" {
   source  = "api.nullstone.io/nullstone/aws-pg-db-admin/aws"
-  version = "~> 0.1.0"
+  version = "~> 0.2.0"
 
   name     = local.resource_name
   tags     = data.ns_workspace.this.tags
