dde30c703af5835ce426ad6ce06ca8b4a480985c
Brad Sickles <brad.sickles@gmail.com>
2021-11-30 10:09:53 -0500

    Fixing high availability toggle

--- db.tf
+++ db.tf
@@ -1,14 +1,14 @@
 resource "aws_db_instance" "this" {
   identifier = local.resource_name
 
-  db_subnet_group_name   = aws_db_subnet_group.this.name
+  db_subnet_group_name   = var.high_availability ? aws_db_subnet_group.this[0].name : ""
   engine                 = "postgres"
   engine_version         = var.postgres_version
   instance_class         = var.instance_class
   multi_az               = true
   allocated_storage      = var.allocated_storage
   storage_encrypted      = true
-  storage_type           = "standard"
+  storage_type           = "gp2"
   port                   = local.port
   vpc_security_group_ids = [aws_security_group.this.id]
 
@@ -35,4 +35,5 @@ resource "aws_db_subnet_group" "this" {
   description = "Postgres db subnet group for postgres cluster"
   subnet_ids  = local.private_subnet_ids
   tags        = data.ns_workspace.this.tags
+  count       = var.high_availability ? 1 : 0
 }
