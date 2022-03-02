03e6fb5f94be0c8e1663512978da79e106dfa588
Brad Sickles <brad.sickles@gmail.com>
2022-03-01 21:14:39 -0500

    name it with postgres

--- db.tf
+++ db.tf
@@ -44,7 +44,7 @@ locals {
 }
 
 resource "aws_db_parameter_group" "this" {
-  name        = "${local.resource_name}-${var.postgres_version}"
+  name        = "${local.resource_name}-postgres${var.postgres_version}"
   family      = "postgres${var.postgres_version}"
   tags        = local.tags
   description = "Postgres for ${local.block_name} (${local.env_name})"
