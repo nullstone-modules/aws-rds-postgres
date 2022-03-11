4875a58ecc1f4504f060699f815c0027537cd693
Brad Sickles <brad.sickles@gmail.com>
2022-03-11 17:32:28 -0500

    Add db instance id output

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "db_instance_arn" {
   description = "string ||| ARN of the Postgres instance"
 }
 
+output "db_instance_id" {
+  value       = aws_db_instance.this.id
+  description = "string ||| ID of the Postgres instance"
+}
+
 output "db_master_secret_name" {
   value       = aws_secretsmanager_secret.password.name
   description = "string ||| The name of the secret in AWS Secrets Manager containing the password"
