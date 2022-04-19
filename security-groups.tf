9f029b797825ebb617ad9654020b88959cf190a4
Brad Sickles <brad.sickles@gmail.com>
2022-04-19 13:59:25 -0400

    Removing unused "user" security group

--- security-groups.tf
+++ security-groups.tf
@@ -4,30 +4,6 @@ resource "aws_security_group" "this" {
   tags   = merge(local.tags, { Name = local.resource_name })
 }
 
-resource "aws_security_group" "user" {
-  vpc_id = local.vpc_id
-  name   = "pg-user/${local.resource_name}"
-  tags   = merge(local.tags, { Name = "pg-user/${local.resource_name}" })
-}
-
-resource "aws_security_group_rule" "this-from-user" {
-  security_group_id        = aws_security_group.this.id
-  protocol                 = "tcp"
-  type                     = "ingress"
-  from_port                = local.port
-  to_port                  = local.port
-  source_security_group_id = aws_security_group.user.id
-}
-
-resource "aws_security_group_rule" "user-to-this" {
-  security_group_id        = aws_security_group.user.id
-  protocol                 = "tcp"
-  type                     = "egress"
-  from_port                = local.port
-  to_port                  = local.port
-  source_security_group_id = aws_security_group.this.id
-}
-
 resource "aws_security_group_rule" "this-from-world" {
   security_group_id = aws_security_group.this.id
   protocol          = "tcp"
