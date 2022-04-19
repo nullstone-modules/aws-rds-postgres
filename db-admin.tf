ca507b48fce0735f1675c268d139df9c413bcd0b
Brad Sickles <brad.sickles@gmail.com>
2022-04-19 15:29:44 -0400

    Fix missing db admin security group

--- db-admin.tf
+++ db-admin.tf
@@ -10,7 +10,32 @@ module "db_admin" {
 
   network = {
     vpc_id             = local.vpc_id
-    security_group_ids = [aws_security_group.user.id]
+    security_group_ids = [aws_security_group.db-admin.id]
     subnet_ids         = local.private_subnet_ids
   }
+}
+
+resource "aws_security_group" "db-admin" {
+  vpc_id      = local.vpc_id
+  name        = "${local.resource_name}/db-admin"
+  tags        = merge(local.tags, { Name = "${local.resource_name}/db-admin" })
+  description = "Security group attached to DB Admin for ${local.resource_name}"
+}
+
+resource "aws_security_group_rule" "db-admin-to-postgres" {
+  security_group_id        = aws_security_group.db-admin.id
+  protocol                 = "tcp"
+  type                     = "egress"
+  from_port                = local.port
+  to_port                  = local.port
+  source_security_group_id = aws_security_group.this.id
+}
+
+resource "aws_security_group_rule" "postgres-from-db-admin" {
+  security_group_id        = aws_security_group.this.id
+  protocol                 = "tcp"
+  type                     = "ingress"
+  from_port                = local.port
+  to_port                  = local.port
+  source_security_group_id = aws_security_group.db-admin.id
 }
