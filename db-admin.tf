b86354f729d3f109090b42c7f6af789290dac666
Brad Sickles <brad.sickles@gmail.com>
2022-04-19 15:36:13 -0400

    Upgraded db-admin module to take in pg_security_group_id

--- db-admin.tf
+++ db-admin.tf
@@ -1,6 +1,6 @@
 module "db_admin" {
   source  = "api.nullstone.io/nullstone/aws-pg-db-admin/aws"
-  version = "~> 0.2.0"
+  version = "~> 0.3.0"
 
   name     = local.resource_name
   tags     = local.tags
@@ -9,33 +9,8 @@ module "db_admin" {
   password = random_password.this.result
 
   network = {
-    vpc_id             = local.vpc_id
-    security_group_ids = [aws_security_group.db-admin.id]
-    subnet_ids         = local.private_subnet_ids
+    vpc_id               = local.vpc_id
+    pg_security_group_id = aws_security_group.this.id
+    subnet_ids           = local.private_subnet_ids
   }
-}
-
-resource "aws_security_group" "db-admin" {
-  vpc_id      = local.vpc_id
-  name        = "${local.resource_name}/db-admin"
-  tags        = merge(local.tags, { Name = "${local.resource_name}/db-admin" })
-  description = "Security group attached to DB Admin for ${local.resource_name}"
-}
-
-resource "aws_security_group_rule" "db-admin-to-postgres" {
-  security_group_id        = aws_security_group.db-admin.id
-  protocol                 = "tcp"
-  type                     = "egress"
-  from_port                = local.port
-  to_port                  = local.port
-  source_security_group_id = aws_security_group.this.id
-}
-
-resource "aws_security_group_rule" "postgres-from-db-admin" {
-  security_group_id        = aws_security_group.this.id
-  protocol                 = "tcp"
-  type                     = "ingress"
-  from_port                = local.port
-  to_port                  = local.port
-  source_security_group_id = aws_security_group.db-admin.id
 }
