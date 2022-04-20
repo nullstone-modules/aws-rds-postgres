a1a65055ac6259cac9097762c3f252e7d9445436
Brad Sickles <brad.sickles@gmail.com>
2022-04-20 09:00:11 -0400

    Fix usage of kms key with cloudwatch logs

--- encryption.tf
+++ encryption.tf
@@ -3,4 +3,42 @@ resource "aws_kms_key" "this" {
   enable_key_rotation = true
   is_enabled          = true
   tags                = local.tags
+  policy              = data.aws_iam_policy_document.encryption_key.json
+}
+
+data "aws_iam_policy_document" "encryption_key" {
+  statement {
+    sid       = "Enable IAM User permissions"
+    effect    = "Allow"
+    resources = ["*"]
+    actions   = ["kms:*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+  }
+
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+    actions = [
+      "kms:Encrypt*",
+      "kms:Decrypt*",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:Describe*"
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["logs.${data.aws_region.this.name}.amazonaws.com"]
+    }
+
+    condition {
+      test     = "ArnEquals"
+      variable = "kms:EncryptionContext:aws:logs:arn"
+      values   = ["arn:aws:logs:${data.aws_region.this.name}:${data.aws_caller_identity.current.account_id}:*"]
+    }
+  }
 }
--- logs.tf
+++ logs.tf
@@ -1,3 +1,7 @@
+locals {
+  logs_group_name = "/aws/rds/instance/${local.resource_name}/postgresql"
+}
+
 resource "aws_cloudwatch_log_group" "this" {
   name              = "/aws/rds/instance/${local.resource_name}/postgresql"
   retention_in_days = 90
