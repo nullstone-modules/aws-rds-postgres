bf6a9c238a17dfac55d344de1e17049ad4f1cae6
Brad Sickles <brad.sickles@gmail.com>
2022-04-20 08:10:49 -0400

    Fixed usage of kms key in logs

--- logs.tf
+++ logs.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_log_group" "this" {
   name              = "/aws/rds/instance/${local.resource_name}/postgresql"
   retention_in_days = 90
-  kms_key_id        = aws_kms_key.this.id
+  kms_key_id        = aws_kms_key.this.arn
   tags              = local.tags
 }
