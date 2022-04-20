a4e6548542de9fea1c55d7b3a900ed381dc590ce
Brad Sickles <brad.sickles@gmail.com>
2022-04-20 08:33:14 -0400

    Only deprecating user security group, not removing

--- db-admin.tf
+++ db-admin.tf
@@ -11,6 +11,7 @@ module "db_admin" {
   network = {
     vpc_id               = local.vpc_id
     pg_security_group_id = aws_security_group.this.id
+    security_group_ids   = [aws_security_group.user.id]
     subnet_ids           = local.private_subnet_ids
   }
 }
