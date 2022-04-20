193068443d112a4f600fc7ea4ee44b929d414c6c
Brad Sickles <brad.sickles@gmail.com>
2022-04-20 09:59:52 -0400

    Added missing upgrade cloudwatch log group

--- encryption.tf
+++ encryption.tf
@@ -20,6 +20,7 @@ data "aws_iam_policy_document" "encryption_key" {
   }
 
   statement {
+    sid       = "Enable Cloudwatch Log encryption"
     effect    = "Allow"
     resources = ["*"]
     actions = [
--- logs.tf
+++ logs.tf
@@ -1,9 +1,12 @@
-locals {
-  logs_group_name = "/aws/rds/instance/${local.resource_name}/postgresql"
-}
-
 resource "aws_cloudwatch_log_group" "this" {
   name              = "/aws/rds/instance/${local.resource_name}/postgresql"
+  retention_in_days = 90
+  kms_key_id        = aws_kms_key.this.arn
+  tags              = local.tags
+}
+
+resource "aws_cloudwatch_log_group" "upgrade" {
+  name              = "/aws/rds/instance/${local.resource_name}/upgrade"
   retention_in_days = 90
   kms_key_id        = aws_kms_key.this.arn
   tags              = local.tags
